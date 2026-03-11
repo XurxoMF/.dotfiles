d39f03648703852393a8e21f128757454aa0840e
XurxoMF <xurxolaxe@gmail.com>
2026-03-11 20:14:27 +0100

    Modificado: Novo estilo de waybar e melloras en muitos temas

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-matugen image --mode dark --type scheme-vibrant --show-colors "$1" | sed 's/\x1B\[[0-9;]*m/ /g' > ~/.dotfiles/cores.txt
+matugen image --mode dark --type scheme-vibrant --verbose --show-colors "$1" | sed 's/\x1B\[[0-9;]*m/ /g' > ~/.dotfiles/cores.txt
